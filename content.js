1c411b6a99a44b21a14ba7919aaa273ea84f9780
Braidy Merkle <braidy.merkle@nordstrom.com>
2021-04-26 19:33:34 -0500

    appends button to same div holding price instead of body

--- content.js
+++ content.js
@@ -28,60 +28,61 @@ function textMatcher(item) {
 }
 
 const firstKeyword = matched[0].toLowerCase();
-console.log("firstkey", firstKeyword);
 
 const displayTextOptions = {
   "whole grains":
-    "Misleading claim alert: Whole grains- \n \n While whole grains are better than refined grains, watch out for added sugars or processed sugars which increase the calorie density of the item, cause inflammation, and have been shown to have a direct link to chronic illnesses. Opt for now added sugar and higher fiber varieties.",
+    "Whole grains- \n While whole grains are better than refined grains, watch out for added sugars or processed sugars which increase the calorie density of the item, cause inflammation, and have been shown to have a direct link to chronic illnesses. Opt for now added sugar and higher fiber varieties.",
   "high fiber":
-    "Misleading claim alert: High fiber- \n \n Marketed as high fiber and healthier, this product might contain added sugars, making it calorie and carb dense, which has been shown to cause blood usgar spikes and leads to cardiovasular disease and insulin resistance if prolonged. ",
+    "High fiber- \n \n Marketed as high fiber and healthier, this product might contain added sugars, making it calorie and carb dense, which has been shown to cause blood usgar spikes and leads to cardiovasular disease and insulin resistance if prolonged. ",
   "gluten free":
-    "Misleading claim alert: Gluten free- \n \n Gluten free is mostly suggested for celiac disease patients and doesn’t carry any additional health advantagesfor others. Further gluten free products have lower dietary fiber, bringing the overall nutritional value down.",
+    "Gluten free- \n \n Gluten free is mostly suggested for celiac disease patients and doesn’t carry any additional health advantagesfor others. Further gluten free products have lower dietary fiber, bringing the overall nutritional value down.",
   "heart healthy":
-    "Misleading claim alert: Heart healthy- \n \n The study supporting this claim has been long challenged by experts for its authenticity. Further, newer studies have shown that lowering cholesterol doesn’t affect heart health. Simple carbs and sugars consumption is directly linked to heart disease.",
+    "Heart healthy- \n \n The study supporting this claim has been long challenged by experts for its authenticity. Further, newer studies have shown that lowering cholesterol doesn’t affect heart health. Simple carbs and sugars consumption is directly linked to heart disease.",
   "lower cholesterol":
-    "Misleading claim alert: Lower cholesterol- \n Study supporting this claim has long been challenged by experts. Further, newer research shows that simple sugars are a major contributor to chronic illnesses as opposed to cholesterol.",
+    "Lower cholesterol- \n Study supporting this claim has long been challenged by experts. Further, newer research shows that simple sugars are a major contributor to chronic illnesses as opposed to cholesterol.",
   "naturally flavored":
-    "Misleading claim alert: naturally flavored- \n The FDA doesn’t have any guidelines for “natural” food. So natural flavoring doesn’t mean healthier or better in any way.",
+    "Naturally flavored- \n The FDA doesn’t have any guidelines for “natural” food. So natural flavoring doesn’t mean healthier or better in any way.",
   "real honey":
-    "Misleading claim alert: Real honey- \n Many ingredients claimed as “real” in processed foods are processed at high temperatures or use processed derivatives of ingredients to increase shelf life and reduce costs – and do not retain all nutritional benefits or taste of the original ingredients.",
+    "Real honey- \n Many ingredients claimed as “real” in processed foods are processed at high temperatures or use processed derivatives of ingredients to increase shelf life and reduce costs – and do not retain all nutritional benefits or taste of the original ingredients.",
   "real cocoa":
-    "Misleading claim alert: Real cocoa- \n Many ingredients claimed as “real” in processed foods are processed at high temperatures or use processed derivatives of ingredients to increase shelf life and reduce costs – and do not retain all nutritional benefits or taste of the original ingredients.",
+    "Real cocoa- \n Many ingredients claimed as “real” in processed foods are processed at high temperatures or use processed derivatives of ingredients to increase shelf life and reduce costs – and do not retain all nutritional benefits or taste of the original ingredients.",
 };
 
 const displayText = displayTextOptions[firstKeyword];
-console.log("textto display", displayText);
 
 function addButtonElement() {
   //create a new div element
   const newButton = document.createElement("button");
-  //give the button content
   newButton.textContent = "Click to Analyze";
-  newButton.className = "taurusButton";
+  newButton.className = "buttonn";
 
   //styling for button
-  newButton.style.background = "#E4572E";
-  newButton.style.position = "relative";
-  newButton.style.top = "18em";
-  newButton.style.left = "46em";
-  newButton.style.zIndex = 8000;
-  newButton.style.color = "white";
-  newButton.style.width = "175px";
-  newButton.style.height = "40px";
-  newButton.style.borderRadius = "10px";
-  newButton.style.padding = "0.5em";
-  newButton.style.boxSizing = "border-box";
+  // newButton.style.background = "#E4572E";
+  // // newButton.style.position = "relative";
+  // newButton.style.top = "18em";
+  // newButton.style.left = "46em";
+  // // newButton.style.zIndex = 8000;
+  // newButton.style.color = "white";
+  // newButton.style.width = "175px";
+  // newButton.style.height = "40px";
+  // newButton.style.borderRadius = "10px";
+  // newButton.style.padding = "0.5em";
+  // newButton.style.boxSizing = "border-box";
 
   //attach button to page
   const currentButton = document.getElementById("headerMain");
   document.body.insertAdjacentElement("afterbegin", newButton, currentButton);
 
-  // document.body.insertAdjacentElement("afterend", debunkModal, newButton);
+  const allDivs = document.getElementsByTagName("div");
+  const buttonContainerNode =
+    allDivs[0].firstChild.childNodes[3].childNodes[1].childNodes[1]
+      .childNodes[1].firstChild.firstChild.firstChild;
+
+  buttonContainerNode.appendChild(newButton);
 
   newButton.onclick = function (e) {
     e.preventDefault();
 
-    const allDivs = document.getElementsByTagName("div");
     const targetNode =
       allDivs[0].firstChild.childNodes[3].childNodes[1].childNodes[1]
         .childNodes[1].childNodes[1];
@@ -89,9 +90,10 @@ function addButtonElement() {
 
     // create the debunking modal
     const debunkModal = document.createElement("div");
-    debunkModal.innerHTML = "Misleading claim alert! <br/> <br/>GLUTEN FREE is mostly suggested for celiac disease patients and doesn't carry any additional health advantages for others. <br/> <br/>This product has 12g of added sugar in a 36g serving size - which is 30% sugar and 40g net carbs.";
+    debunkModal.innerHTML = `Misleading claim alert! <br/><br/>${displayText}<br/>`;
     debunkModal.style.background = "#fefefe";
-    debunkModal.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+    debunkModal.style.boxShadow =
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
     debunkModal.style.borderRadius = "2px";
     debunkModal.style.padding = "12px";
     debunkModal.style.height = "300px";
@@ -100,8 +102,7 @@ function addButtonElement() {
     if (newButton.classList.contains("button-showing-debunk")) {
       newButton.classList.remove("button-showing-debunk");
       newButton.textContent = "Click to Analyze";
-      newButton.style.background =
-        "#E4572E";
+      newButton.style.background = "#E4572E";
       const modalChild = targetNode.lastChild;
       targetNode.removeChild(modalChild);
     } else {
@@ -111,10 +112,6 @@ function addButtonElement() {
 
       targetNode.appendChild(debunkModal);
     }
-
-    console.log(
-      "Heads up!! Labeling like 'Gluten Free' has no real health benefit unless you're celiac!"
-    );
   };
 }
 
